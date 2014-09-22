8a2d2247cafdb1d01904422d959fb42ea3d30e19
xedoc <anton.meshkov@gmail.com>
2014-09-22 23:46:31 +0300

    Design time data for music ticker

--- Ubiquitous2/Design/GeneralDataServiceDesign.cs
+++ Ubiquitous2/Design/GeneralDataServiceDesign.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using UB.Model;
+using UB.Properties;
 
 namespace UB.Design
 {
-    class GeneralDataServiceDesign : IGeneralDataService
+    public class GeneralDataServiceDesign : IGeneralDataService
     {
         public GeneralDataServiceDesign()
         {
-            Services = new List<IService>();
+            Services = new List<IService>() {
+                new LastFMServiceDesign( new ServiceConfig() { 
+                    ServiceName = SettingsRegistry.ServiceTitleMusicTicker,
+                    Enabled = true,
+                })
+            };
+
         }
         public List<IService> Services
         {
@@ -32,5 +43,59 @@ namespace UB.Design
         {
             return null;
         }
+    }
+
+    public class LastFMServiceDesign : IService
+    {
+        public LastFMServiceDesign( ServiceConfig config)
+        {
+            Config = config;
+        }
+        public bool Start()
+        {
+            return true;
+        }
+
+        public bool Stop()
+        {
+            return false;
+        }
+
+        public void Restart()
+        {
+            
+        }
+
+        public Action AfterStart
+        {
+            get;
+            set;
+
+        }
+
+        public ServiceConfig Config
+        {
+            get;
+            set;
+        }
+
+        public StatusBase Status
+        {
+            get;
+            set;
+
+        }
+
+        public void GetData(Action<object> callback)
+        {
+            callback( new MusicTrackInfo()
+            {
+                Album = "Lorem ipsum",
+                Artist = "Dolor sit amet",
+                Title = "Consectetur adipiscing elit, sed do eiusmod tempor",
+                ImageURL = Icons.MainHeadsetIcon,
+            });
+        }
+
     }
 }
--- Ubiquitous2/Model/Icons.cs
+++ Ubiquitous2/Model/Icons.cs
@@ -8,16 +8,16 @@ namespace UB.Model
 {
     public static class Icons
     {
-        public static string MainIcon = @"/Ubiquitous2;component/Resources/ubiquitous smile.ico";
-        public static string MainHeadsetIcon = @"/Ubiquitous2;component/Resources/ubiheadset.png";
-        public static string TwitchEventIcon = @"/Ubiquitous2;component/Resources/twitchevent.ico";
-        public static string TwitchIcon = @"/Ubiquitous2;component/Resources/twitch.ico";
-        public static string LastFMIcon = @"/Ubiquitous2;component/Resources/lastfm.ico";
-        public static string WebServerIcon = @"/Ubiquitous2;component/Resources/webserver.png";
-        public static string GamingLiveIcon = @"/Ubiquitous2;component/Resources/gaminglive.png";
-        public static string GamingLiveIconDark = @"/Ubiquitous2;component/Resources/gaminglivedark.ico";
-        public static string HitboxIcon = @"/Ubiquitous2;component/Resources/hitbox.png";
-        public static string SteamIcon = @"/Ubiquitous2;component/Resources/steam.ico";
-        public static string PngIcon = @"/Ubiquitous2;component/Resources/png.png";
+        public static string MainIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/ubiquitous smile.ico";
+        public static string MainHeadsetIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/ubiheadset.png";
+        public static string TwitchEventIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/twitchevent.ico";
+        public static string TwitchIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/twitch.ico";
+        public static string LastFMIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/lastfm.ico";
+        public static string WebServerIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/webserver.png";
+        public static string GamingLiveIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/gaminglive.png";
+        public static string GamingLiveIconDark = @"pack://application:,,,/Ubiquitous2;component/Resources/gaminglivedark.ico";
+        public static string HitboxIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/hitbox.png";
+        public static string SteamIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/steam.ico";
+        public static string PngIcon = @"pack://application:,,,/Ubiquitous2;component/Resources/png.png";
     }
 }
