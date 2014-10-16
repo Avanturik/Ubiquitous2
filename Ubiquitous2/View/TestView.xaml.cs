46b5782e0fe3a8bbd7a162d02f42af4f15decffb
xedoc <anton.meshkov@gmail.com>
2014-10-16 07:00:58 +0300

    starcitizen theme - icons

---
+++ Ubiquitous2/View/TestView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UB
+{
+	/// <summary>
+	/// Interaction logic for TestView.xaml
+	/// </summary>
+	public partial class TestView : Window
+	{
+		public TestView()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
